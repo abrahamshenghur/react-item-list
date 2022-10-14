52fdedbf66b4c5822b53f959b2df4ddb9f0cff87
abrahamshenghur <ashenghur@gmail.com>
2022-10-14 12:06:32 -0700

    Setup basic UI components

---
+++ src/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Content = () => {
+    const handleNameChange = () => {
+        const names = ['Bob', 'dave', 'tim'];
+        const int = Math.floor(Math.random() * 3);
+        console.log(int)
+        return names[int];
+    }
+      
+  return (
+    <main>
+        <p>Hello {handleNameChange()}</p>
+    </main>
+  )
+}
+
+export default Content
