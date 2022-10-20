04fee429b9aabf82c75385c9116a3986f0a1ed75
abrahamshenghur <ashenghur@gmail.com>
2022-10-20 00:12:03 -0700

    Create controlled search input component having functionality with filter()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from './Header';
 import AddItem from './AddItem';
+import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react'
 
 function App() {
   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')))
-
   const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems)
@@ -46,8 +47,12 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit} 
       />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete} />
       <Footer length={items.length} />
