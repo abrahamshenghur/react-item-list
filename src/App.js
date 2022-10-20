378b15502fb493315435b8c38a8fdd3b0c24a80d
abrahamshenghur <ashenghur@gmail.com>
2022-10-19 23:21:00 -0700

    Define addItem func called from handleSubmit

--- src/App.js
+++ src/App.js
@@ -25,6 +25,14 @@ function App() {
 
   const [newItem, setNewItem] = useState('')
 
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1
+    const myNewItem = { id, checked: false, item }
+    const listItems = [...items, myNewItem]
+    setItems(listItems)
+    localStorage.setItem('shoppingList', JSON.stringify(listItems))
+  }
+
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
@@ -40,7 +48,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!newItem) return
-    // addItem
+    addItem(newItem)
     setNewItem('')
   }
 
